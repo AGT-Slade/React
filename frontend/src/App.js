1cad9a07b225383e87df724ad717fc40bc601651
AGT-Slade <joseandez17@gmail.com>
2024-01-12 09:44:34 -0600

    Video-05-List-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">React</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((productsArray) => (
+            <div className="product" key={productsArray.slug}>
+              <a href={`/product/${productsArray.slug}`}>
+                <img src={productsArray.image} alt={productsArray.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${productsArray.slug}`}>
+                  <p>{productsArray.name}</p>
+                </a>
+                <p>
+                  <strong>${productsArray.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
